a12b9b2153d242e038665c2716e4072ead59ecf6
Nicholas Equitz <nequitz1@gmail.com>
2026-03-26 02:47:24 -0500

    Remove unused axios import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
