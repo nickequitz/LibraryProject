9b79408c73c0edab99af5aad8883ced5764b5ee1
Nicholas Equitz <nequitz1@gmail.com>
2026-03-26 04:09:57 -0500

    Fix cross-domain session cookies for production

--- server.js
+++ server.js
@@ -19,10 +19,11 @@ app.use(session({
     secret: "secret_key", //need to implement a better secret key later for logged in session security
     resave: false,
     saveUninitialized: false,
+    proxy: true,
     cookie: {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 1000 * 60 * 60 * 24 //session lasts 1 day
     }
 }))
